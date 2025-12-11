a7111df4c6f8bbd3e92396b946ee95019311843d
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2025-12-11 21:09:30 +0100

    update java-docs

--- src/pr1/a08/Test.java
+++ src/pr1/a08/Test.java
@@ -7,12 +7,12 @@ import pr1.a07.plot.PlotSet;
 public class Test {
 
     public static void main(String[] args) {
-        einfacheLandschaft();
-        landschaft();
-        winterLandschaft();
+        testEinfacheLandschaft();
+        testLandschaft();
+        testWinterLandschaft();
     }
 
-    public static void einfacheLandschaft() {
+    public static void testEinfacheLandschaft() {
         PlotApplication app = new PlotApplication("Einfache Landschaft", 600, 400);
         PlotSet<EinfacheLandschaft> set = new PlotSet<>() {
         };
@@ -22,17 +22,18 @@ public class Test {
         app.start();
     }
 
-    public static void landschaft() {
+    public static void testLandschaft() {
         PlotApplication app = new PlotApplication("Landschaft", 600, 400);
         PlotSet<EinfacheLandschaft> set = new PlotSet<>() {
         };
         set.setGrid(new EmptyGrid());
         set.addGraph(new Landschaft(3, 12));
+        set.createControl(app);
         app.addPlotSet(set);
         app.start();
     }
 
-    public static void winterLandschaft() {
+    public static void testWinterLandschaft() {
         PlotApplication app = new PlotApplication("Winterlandschaft", 600, 400);
         PlotSet<EinfacheLandschaft> set = new PlotSet<>() {
         };
