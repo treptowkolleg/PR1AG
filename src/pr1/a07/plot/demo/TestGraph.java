6b627638ae5bffc46960ec2710d39dc8358ae99f
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2025-12-11 09:21:54 +0100

    update

--- src/pr1/a07/plot/demo/TestGraph.java
+++ src/pr1/a07/plot/demo/TestGraph.java
@@ -41,6 +41,6 @@ public class TestGraph extends PlotGraph<TestGraph> {
     @Override
     public void configureGraphics2D(Graphics2D g) {
         g.setColor(color);
-        g.drawRect(centerX - 50 + x, centerY - 50 + y, 100, 100);
+        g.drawRect(centerX - 50 + x, (centerY - 50 - y), 100, 100);
     }
 }
