898968fb7aaaed80cc103053df3084a620346f80
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2025-12-13 22:26:47 +0100

    update

--- src/pr1/a07/plot/DrawableObject.java
+++ src/pr1/a07/plot/DrawableObject.java
@@ -34,7 +34,7 @@ import java.awt.Graphics2D;
  *
  * <p>For any reuse beyond the original course setting (e.g., in personal projects, open-source
  * repositories, or other educational contexts), you <em>must</em> create a project-local
- * alternative interface (e.g., {@code your.package.Drawable}) and refactor all implementing
+ * alternative interface (e.g., {@code your.package.Drawable#void draw(Graphics g);}) and refactor all implementing
  * classes to depend on that instead.</p>
  */
 public abstract class DrawableObject implements Drawable, CustomDrawable {
