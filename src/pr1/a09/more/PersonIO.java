30dbc03d7b6b43a83614928a8be405cc37becb7d
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2025-12-24 11:35:05 +0100

    update

--- src/pr1/a09/more/PersonIO.java
+++ src/pr1/a09/more/PersonIO.java
@@ -30,7 +30,7 @@ public class PersonIO extends IOApplication {
         for (Map.Entry<String, ObjectFactory<?>> entry : PEOPLE.entrySet()) {
             decorator.printHeadline(entry.getKey());
             withFileScanner("people.txt", scanner -> {
-                Factory.listFromInput(scanner, entry.getValue())
+                Factory.listOfInput(scanner, entry.getValue())
                         .forEach(this::println);
             });
         }
